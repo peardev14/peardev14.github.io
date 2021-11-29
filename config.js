14a839f7b18bf324b7c4e85f4c49d61fe77af574
peardev14 <95229501+peardev14@users.noreply.github.com>
2021-11-29 23:41:38 +0700

    upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Chào cô gái của anh',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Anh biết Em thích Anh đúng khôngggg????',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :>> ',
+    btnYes: 'Có, em thật sự thích anh <33',
+    btnNo: ' Khôngggggggg ! Anh mơ à !  ',
+    question:'Nói cho Anh biết đi vì sao Em thích anh đi??? :>>>',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gửiii ! <333',
+    reply: 'Tại vì anh đẹp trai đó Ahiiiii ',
+    mess: 'Anh biết mà, Anh cũng thích em lắm . Yêu Em Kkkkk',
+    messDesc: ' Nhắn tin cho Anh nhéeeee. ',
+    btnAccept: 'Ok lun ',
+    messLink: 'https://m.me/letienthanh423'
+}
